490d2d7de5a90f6f4605fa4a6ddc05d3d4c7f8e6
maurinei-miranda <maurinei.develop@gmail.com>
2021-11-17 21:20:59 -0300

    Update gitignore
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

---
+++ modulo1/src/cadastroDeLivro.java
@@ -0,0 +1,10 @@
+public class pessoa {
+    private String nome;
+    public int idade;
+    private int peso;
+
+    public int getIdade() {
+        System.out.println("A idade do joao e: "+ this.idade);
+        return this.idade;
+    }
+}
